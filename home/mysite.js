36b992625d1bffd80bb1d711e5ea89a50d62cc8a
risako0703] <risako070310@gmail.com>
2020-01-19 18:02:40 +0900

    add animation

---
+++ home/mysite.js
@@ -0,0 +1,9 @@
+$(function(){
+    var animationTivel = lottie.loadAnimation({
+        container: document.getElementById('animation'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'home/data.json'
+    }); 
+});
